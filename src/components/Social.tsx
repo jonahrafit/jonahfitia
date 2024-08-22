47708645fd9c06792dfe324bf05ebddea234d436
jonahrafit <jonahrafit.ram@hotmail.com>
2024-08-22 12:35:53 +0300

    finalisation page d acceuil

---
+++ src/components/Social.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaFacebook, FaVoicemail, FaEnvelope } from "react-icons/fa";
+
+interface SocialProps {
+    containerStyles: string;
+    iconStyles: string;
+}
+
+const socials = [
+    { icon: <FaGithub />, path: 'https://www.github.com/jonahrafit/' },
+    { icon: <FaLinkedin />, path: 'https://www.linkedin.com/in/ramanantsafidy' },
+    { icon: <FaFacebook />, path: 'https://www.facebook.com/jonah.rafit/' }
+];
+
+const Social: React.FC<SocialProps> = ({ containerStyles, iconStyles }) => {
+    return (
+        <div className={containerStyles}>
+            {socials.map((item, index) => {
+                return (
+                    <Link key={index} href={item.path} className={iconStyles}>
+                        {item.icon}
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Social;
