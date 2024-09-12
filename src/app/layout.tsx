781ef9f2fa3663c55f9b87147736bb3dc7a449e5
jonahrafit <jonahrafit.ram@hotmail.com>
2024-09-12 20:32:47 +0300

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-"use client";
-
 import { JetBrains_Mono } from "next/font/google";
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 import { LanguageProvider } from "@/context/LanguageContext";
+import Script from "next/script";  // Import Script de next/script
 
 const jetBrains_Mono = JetBrains_Mono({
   subsets: ["latin"],
@@ -16,11 +15,28 @@ const jetBrains_Mono = JetBrains_Mono({
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;// Ajout de locale ici
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={jetBrains_Mono.className}>
+        {/* Google Analytics */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TLWP2ZDQMD" />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TLWP2ZDQMD', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+
         <LanguageProvider>
           <Header />
           <StairTransition />
